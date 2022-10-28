ca697209126cd9b9737b02d8deaeb442ddeeaa57
xuanhieuvnhd <xuanhieuvn@gmail.com>
2022-10-28 12:19:42 +0700

    28/10

---
+++ src/main/java/com/repository/ICustomerRepository.java
@@ -0,0 +1,9 @@
+package com.repository;
+
+import com.model.Customer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+}
---
+++ src/main/java/com/service/customer/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.service.customer;
+
+import com.model.Customer;
+import com.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
